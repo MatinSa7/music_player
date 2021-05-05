d79794e090ba3e3d84f961c3bf22a7a306ef119b
Matin Sarahi <matinsarahi@gmail.com>
2021-05-05 18:15:48 -0400

    Library Shenanigans

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles/app.scss";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 //Import util
 import data from "./util";
 
@@ -11,10 +12,16 @@ function App() {
   //State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} />
     </div>
   );
 }
