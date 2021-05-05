7b60340bdb19e0221bc79cc3c5cd1e841335a20c
Matin Sarahi <matinsarahi@gmail.com>
2021-05-05 18:49:57 -0400

    library css

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -4,8 +4,10 @@ const LibrarySong = ({ song }) => {
   return (
     <div className="library-song">
       <img alt={song.name} src={song.cover}></img>
-      <h2>{song.name}</h2>
-      <h3>{song.artist}</h3>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
     </div>
   );
 };
