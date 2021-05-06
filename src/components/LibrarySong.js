e44031335e1c64a69092f054c0443ca6ca530483
Matin Sarahi <matinsarahi@gmail.com>
2021-05-06 16:51:42 -0400

    Shenanigans

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -7,9 +7,26 @@ const LibrarySong = ({
   id,
   audioRef,
   isPlaying,
+  setSongs,
 }) => {
   const selectedSonghandler = async () => {
     await setCurrentSong(song);
+    const newSongs = songs.map((song) => {
+      if(song.id === id){
+        return {
+          ...song, 
+          active: true,
+        }
+      }else {
+      return {
+        ...song, 
+        active: false,
+      };
+     }
+    });
+    
+    setSongs(newSongs);
+
     if (isPlaying) {
       const playPromise = audioRef.current.play();
       if (playPromise !== undefined) {
@@ -23,7 +40,7 @@ const LibrarySong = ({
   };
 
   return (
-    <div onClick={selectedSonghandler} className="library-song">
+    <div onClick={selectedSonghandler} className={`library-song ${song.active ? 'selected' : ""}`}>
       <img alt={song.name} src={song.cover}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
